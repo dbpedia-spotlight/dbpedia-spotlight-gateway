1c41a328700982ab09691f47daf40e1e3d0cc818
sandro.coelho <sandro.coelho@afferolab.com.br>
2016-12-12 22:47:48 -0200

    Initial commit

---
+++ src/main/java/org/dbpedia/spotlight/Gateway.java
@@ -0,0 +1,21 @@
+package org.dbpedia.spotlight;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+
+@EnableZuulProxy
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+@IntegrationComponentScan
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
+public class Gateway {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class, args);
+    }
+}
